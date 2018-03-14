affa94c67925a1db95c5b2e103feb0675985b94a
Maximilian Stoiber <contact@mxstbr.com>
2018-03-14 11:04:11 +0100

    feat(core): add support for multiple console statements in a file

--- src/index.js
+++ src/index.js
@@ -1,26 +1,34 @@
-const PATTERN = /console\.(log|error|warn|info)/g;
+const PATTERN = /console\.(log|error|warn|info)/
+const GLOBAL_PATTERN = new RegExp(PATTERN.source, 'g')
 
 /**
  * Danger plugin to prevent merging code that still has `console.log`s inside it.
  */
 export default async function noConsole(options = {}) {
-  const whitelist = options.whitelist || [];
-  if (!Array.isArray(whitelist)) throw new Error('[danger-plugin-no-console] whitelist option has to be an array.');
+  const whitelist = options.whitelist || []
+  if (!Array.isArray(whitelist))
+    throw new Error(
+      '[danger-plugin-no-console] whitelist option has to be an array.',
+    )
 
   const files = danger.git.modified_files.concat(danger.git.created_files)
   const contents = await Promise.all(
-    files.map(file => danger.github.utils.fileContents(file).then(content => ({
-      file,
-      content,
-    })))
+    files.map(file =>
+      danger.github.utils.fileContents(file).then(content => ({
+        file,
+        content,
+      })),
+    ),
   )
 
   contents.forEach(({ file, content }) => {
-    let matches = content.match(PATTERN);
-    if (!matches) return;
-    matches = matches.filter(match => !whitelist.includes(PATTERN.exec(match)[1]));
-    if (matches.length === 0) return;
+    let matches = content.match(GLOBAL_PATTERN)
+    if (!matches) return
+    matches = matches.filter(
+      match => !whitelist.includes(PATTERN.exec(match)[1]),
+    )
+    if (matches.length === 0) return
 
-    fail(`${matches.length} console statement(s) left in ${file}.`);
+    fail(`${matches.length} console statement(s) left in ${file}.`)
   })
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,26 +1,31 @@
-import noConsole from './';
+import noConsole from './'
 
 const files = {
-  'src/log.js': 'function add(a, b) {\n  console.log(a, b);\n  return a + b;\n}',
+  'src/log.js':
+    'function add(a, b) {\n  console.log(a, b);\n  return a + b;\n}',
   'src/clean.js': 'function add(a, b) {\n  return a + b;\n}',
-  'src/error.js': 'function add(a, b) {\n  console.error(a, b);\n  return a + b;\n}',
+  'src/error.js':
+    'function add(a, b) {\n  console.error(a, b);\n  return a + b;\n}',
+  'src/multiple.js':
+    'function add(a, b) {\n  console.info(a, b);\n  console.info(b, a);\n  return a + b;\n}',
 }
 
-const fileNames = Object.keys(files);
+const fileNames = Object.keys(files)
 
 // Mock the Danger API
 global.danger = {
   git: {
     modified_files: [fileNames[0], fileNames[1]],
-    created_files: [fileNames[2]]
+    created_files: [fileNames[2], fileNames[3]],
   },
   github: {
     utils: {
-      fileContents: (path) => new Promise(res => {
-        res(files[path])
-      })
-    }
-  }
+      fileContents: path =>
+        new Promise(res => {
+          res(files[path])
+        }),
+    },
+  },
 }
 
 beforeEach(() => {
@@ -33,12 +38,18 @@ afterEach(() => {
 
 describe('noConsole()', () => {
   it('should fail any files with a console.log in it', async () => {
-    await noConsole();
-    expect(global.fail).toHaveBeenCalledTimes(2);
-  });
+    await noConsole()
+    expect(global.fail).toHaveBeenCalledTimes(3)
+  })
 
   it('should respect the whitelist of console properties', async () => {
-    await noConsole({ whitelist: ['error'] });
-    expect(global.fail).toHaveBeenCalledTimes(1);
+    await noConsole({ whitelist: ['error'] })
+    expect(global.fail).toHaveBeenCalledTimes(2)
+  })
+
+  it('should handle multiple console statements in a file', async () => {
+    await noConsole({ whitelist: ['error', 'warn', 'log'] })
+    expect(global.fail).toHaveBeenCalledTimes(1)
+    expect(global.fail.mock.calls[0][0].startsWith(2))
   })
 })
